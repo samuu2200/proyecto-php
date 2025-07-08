b2c9df674fe8d991fc779a37a31ce22045602a30
samdev2200 <samuel2200@hotmail.es>
2025-07-08 16:11:45 +0200

    Sistema de ficheros con php

---
+++ src/public/aprendiendo-php/20-ficheros/index.php
@@ -0,0 +1,32 @@
+<?php 
+// Definir la ruta del archivo
+$archivoRuta = __DIR__ . "/fichero.txt";
+
+$archivo = fopen($archivoRuta, "a+");
+if (!$archivo) {
+    die("Error: No se pudo abrir el archivo");
+}
+
+// Poner el puntero al principio para leer todo
+rewind($archivo);
+
+// Leer y mostrar el contenido de manera segura
+echo "<pre>"; // Para que el texto conserve los de línea
+while (!feof($archivo)) {
+    $linea = fgets($archivo);
+    if ($linea !== false) {
+        echo htmlspecialchars($linea); // Evitamos inyecciones html
+    }
+}
+echo "</pre>";
+
+// Escribir en el archivo
+$texto = "*** Soy el archivo metido desde PHP ***" . PHP_EOL;
+if (fwrite($archivo, $texto) === false) {
+    echo "Error: No se pudo escribir en el archivo";
+}
+
+// Cerrar el archivo
+fclose($archivo);
+
+?>
